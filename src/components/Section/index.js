2c798c606c15b484336ef709e6e25d19475aa0a7
Killian <mkillian.development@gmail.com>
2021-11-06 10:41:04 +0100

    add home items

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
 import './styles.css'
-import Arrow from '../../images/down-arrow.svg'
 
-function Section() {
+function Section({
+  title,
+  description,
+  btnLeftText,
+  btnRightText,
+  background,
+}) {
+  const ProductContainer = styled.div`
+    background-image: ${(props) => `url("/images/${props.backgroundImg}")`};
+  `
+
   return (
-    <div className="section">
+    <ProductContainer className="section" backgroundImg={background}>
       <div className="section__text">
-        <h1>Model 3</h1>
-        <p>Réservez un essai sans contact</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </div>
       <div className="section__btn">
         <div className="btn__group">
-          <button className="btn__left">Configuration personnalisée</button>
-          <button className="btn__right">Véhicules disponibles</button>
+          <button className="btn__left">{btnLeftText}</button>
+          {btnRightText && (
+            <button className="btn__right">{btnRightText}</button>
+          )}
         </div>
-        <img src={Arrow} className="btn__arrow" alt="Animate arrow" />
+        <img
+          src="/images/down-arrow.svg"
+          className="btn__arrow"
+          alt="Animate arrow"
+        />
       </div>
-    </div>
+    </ProductContainer>
   )
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Section from '../../components/Section'
+
+function Home() {
+  return (
+    <div className="Home">
+      <Section
+        title="Model 3"
+        description="Réservez un essai sans contact"
+        background="../../images/model-3.jpg"
+        btnLeftText="Configuration personnalisée"
+        btnRightText="Véhicules disponibles"
+      />
+      <Section
+        title="Model Y"
+        background="model-y.jpg"
+        btnLeftText="Configuration personnalisée"
+        btnRightText="Essais"
+      />
+      <Section
+        title="Model S"
+        background="model-s.jpg"
+        btnLeftText="Configuration personnalisée"
+        btnRightText="Véhicules disponibles"
+      />
+      <Section
+        title="Model X"
+        background="model-x.jpg"
+        btnLeftText="Configuration personnalisée"
+        btnRightText="Véhicules disponibles"
+      />
+      <Section
+        title="Systèmes d'énergie solaire et Powerwalls"
+        description="De l’énergie pour tous vos besoins"
+        background="solar-panel.jpg"
+        btnLeftText="En savoir plus"
+      />
+      <Section
+        title="Accessoires"
+        background="accessories.jpg"
+        btnLeftText="Commander maintenant"
+      />
+    </div>
+  )
+}
+
+export default Home
