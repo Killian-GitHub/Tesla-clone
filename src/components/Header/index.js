51198832753eb5f44fe4378b124bcd38395f8362
Killian <mkillian.development@gmail.com>
2021-11-06 20:40:37 +0100

    update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,47 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './styles.css'
 
 function index() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header">
+      <a href="/">
+        <img src="/images/logo.svg" alt="Tesla logo" />
+      </a>
+      <div className="header__menu">
+        <p>
+          <a href="/">Model S</a>
+        </p>
+        <p>
+          <a href="/">Model 3</a>
+        </p>
+        <p>
+          <a href="/">Model X</a>
+        </p>
+        <p>
+          <a href="/">Model Y</a>
+        </p>
+        <p>
+          <a href="/">Powerwall</a>
+        </p>
+        <p>
+          <a href="/">Recharge</a>
+        </p>
+      </div>
+      <div className="header__nav">
+        <p>
+          <a href="/">Assistance</a>
+        </p>
+        <p>
+          <a href="/">Shop</a>
+        </p>
+        <p>
+          <a href="/">Compte</a>
+        </p>
+        <p>
+          <a href="/">Menu</a>
+        </p>
+      </div>
     </div>
   )
 }
